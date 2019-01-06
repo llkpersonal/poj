0658f5d8f0cc2b220225f93c16a801be27a3283e
Lingkun <llkpersonal@126.com>
2019-01-07 01:07:23 +0800

    Modified 1024.cc.

--- 1024.cc
+++ 1024.cc
@@ -10,6 +10,7 @@ using namespace std;
 typedef set< pair< pair<int,int>, pair<int,int> > > wallset;
 
 char path[10010];
+int f[110][110];
 
 bool check_boundary(int w, int h, int x, int y) {
     return x >= 0 && x < w && y >= 0 && y < h;
@@ -50,7 +51,6 @@ pair<int,int> check_walls_and_get_terminal(int w, int h, wallset& walls) {
         int dx = d.first, dy = d.second;
         int x = res.first, y = res.second;
         int nx = x + dx, ny = y + dy;
-        // printf("nx = %d, ny = %d\n", nx, ny);
         if(not_wall(x, y, nx, ny, walls)) {
             if(check_boundary(w, h, nx, ny)) {
                 res = make_pair(nx, ny);
@@ -66,70 +66,75 @@ pair<int,int> check_walls_and_get_terminal(int w, int h, wallset& walls) {
 }
 
 bool check_unique_and_shortest(int tx, int ty, int w, int h, wallset& walls) {
-    //printf("WALLS COUNT: %d\n", walls.size());
     static int dx[] = {-1, 1, 0, 0};
     static int dy[] = {0, 0, 1, -1};
+    for(int i = 0; i < w; i++) {
+        for(int j = 0; j < h; j++) {
+            f[i][j] = -1;
+        }
+    }
     int path_length = strlen(path);
-    queue< pair< pair<int, int>, int> > q;
-    q.push(make_pair(make_pair(0,0),0));
-    map< pair<int, int>, pair< pair<int,int>, int> > visited;
-    visited[make_pair(0, 0)] = make_pair(make_pair(-1, -1), 0);
-    int shortest_length = 0;
-    
+    int count = 0;
+    queue< pair<int, int> > q;
+    f[0][0] = 0;
+    q.push(make_pair(0, 0));
     while(!q.empty()) {
-        pair< pair<int, int>, int> t = q.front(); q.pop();
-        pair<int, int> position = t.first;
-        //printf("(%d, %d)\n", position.first, position.second);
-        int step = t.second;
+        pair<int, int> t = q.front(); q.pop();
+        int x = t.first, y = t.second;
+        int step = f[x][y];
         if(step > path_length) continue;
-        if(position.first == tx && position.second == ty) {
+        if(x == tx && y == ty) {
             if(step < path_length) return false;
-            shortest_length ++;
-            if(shortest_length == 2) return false;
+            ++count;
+            if(count == 2) return false;
         }
         for(int i = 0; i < 4; i++) {
-            int nx = position.first + dx[i];
-            int ny = position.second + dy[i];
-            if(not_wall(position.first, position.second, nx, ny, walls) && check_boundary(w, h, nx, ny)) {
-                map< pair<int, int>, pair< pair<int, int>, int> >::iterator it
-                    = visited.find(make_pair(nx, ny));
-                if(it == visited.end() ||
-                   (it != visited.end() && it->second.second == step+1 &&
-                        (it->second.first.first != nx || it->second.first.second != ny))
-                   ){
-                    visited[make_pair(nx, ny)] = make_pair(position, step+1);
-                    q.push(make_pair(make_pair(nx, ny), step+1));
-                } else if(it->second.second > step + 1) {
-                    //printf("nx = %d, ny = %d, it->second = %d\n", nx, ny, it->second.second);
-                    return false;
-                }
+            int nx = x + dx[i], ny = y + dy[i];
+            if(check_boundary(w, h, nx, ny) &&
+                not_wall(x, y, nx, ny, walls) && f[nx][ny] == -1) {
+                f[nx][ny] = step+1;
+                q.push(make_pair(nx, ny));
             }
         }
     }
     
-    return shortest_length == 1;
+    if(count != 1) return false;
+    
+    int previous_x = -1, previous_y = -1;
+    while(tx != 0 || ty != 0) {
+        int cnt = 0;
+        int xx = tx, yy = ty;
+        for(int i = 0; i < 4; i++) {
+            int nx = xx + dx[i], ny = yy + dy[i];
+            if((nx != previous_x || ny != previous_y) &&
+                check_boundary(w, h, nx, ny) && f[xx][yy] == f[nx][ny] + 1) {
+                ++cnt;
+                tx = nx;
+                ty = ny;
+            }
+        }
+        previous_x = tx;
+        previous_y = ty;
+        if(cnt > 1) return false;
+    }
+    
+    return true;
 }
 
 bool check_correct(int w, int h, wallset& walls) {
     pair<int, int> terminal = check_walls_and_get_terminal(w, h, walls);
     if(terminal.first == -1) {
-        // printf("CASE 1: walls invalid!\n");
         return false;
     }
-    // printf("CASE 1 PASSED!\n");
     
     if(!check_unique_and_shortest(terminal.first, terminal.second, w, h, walls)) {
-        // printf("CASE 2: unique or shortest invalid!\n");
         return false;
     }
-    // printf("CASE 2 PASSED!\n");
-    // return false;
+    
     for(wallset::iterator it = walls.begin(); it != walls.end(); ++it) {
         pair< pair<int,int>, pair<int,int> > t = *it;
         walls.erase(it);
-        //printf("Remove wall: (%d, %d) | (%d, %d)\n", t.first.first, t.first.second, t.second.first, t.second.second);
         if(check_unique_and_shortest(terminal.first, terminal.second, w, h, walls)) {
-            //printf("CASE 3: extra walls!\n");
             return false;
         }
         walls.insert(t);
