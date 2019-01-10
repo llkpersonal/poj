40a9b31f1b6783ae12f674d5de52afef590bb810
lingkun <llkpersonal@126.com>
2019-01-10 19:05:51 +0800

    Accepted code of 1027.

--- 1027.cc
+++ 1027.cc
@@ -1,21 +1,24 @@
+#include <algorithm>
 #include <cstdio>
 #include <cstring>
 #include <vector>
 using namespace std;
 
 char matrix[10][20];
-char t_matrix[10][20];
+bool vis[10][20];
 
-bool valid_position(int x, int y, char m[10][20]) {
-    return x >= 0 && x < 10 && y >= 0 && y < 20 && m[x][y] != '.';
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+bool valid_position(int x, int y) {
+    return x >= 0 && x < 10 && y >= 0 && y < 20;
 }
 
 void find_max_cluster(int x,
                       int y,
                       int& chess_count,
                       int& left_most,
-                      int& bottom_most,
-                      char m[10][20]) {
+                      int& bottom_most) {
     if(left_most == -1 || y < left_most) {
         left_most = y;
         bottom_most = x;
@@ -23,22 +26,30 @@ void find_max_cluster(int x,
     if(y == left_most && x < bottom_most) {
         bottom_most = x;
     }
+    vis[x][y] = true;
     chess_count++;
-    char t = m[x][y];
-    m[x][y] = '.';
-    static int dx[] = {-1, 1, 0, 0};
-    static int dy[] = {0, 0, -1, 1};
     for(int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
-        if(valid_position(nx, ny, m) && m[nx][ny] == t) {
-            find_max_cluster(nx, ny, chess_count, left_most, bottom_most, m);
+        if(valid_position(nx, ny) && !vis[nx][ny] && matrix[nx][ny] == matrix[x][y]) {
+            find_max_cluster(nx, ny, chess_count, left_most, bottom_most);
         }
     }
 }
 
+void fill_dot(int x, int y, char c) {
+    matrix[x][y] = '.';
+    for(int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(valid_position(nx, ny) && matrix[nx][ny] == c) {
+            fill_dot(nx, ny, c);
+        }
+    }
+}
 
 void regular_column(int y, vector<char>& v) {
+    v.clear();
     for(int x = 0; x < 10; x++) {
         if(matrix[x][y] != '.') {
             v.push_back(matrix[x][y]);
@@ -47,18 +58,22 @@ void regular_column(int y, vector<char>& v) {
 }
 
 void regular_chessboard() {
-    for(int i = 0; i < 10; i++) for(int j = 0; j < 15; j++) t_matrix[i][j] = '.';
     int y = 0;
+    vector<char> v;
     for(int j = 0; j < 15; j++) {
-        vector<char> v;
         regular_column(j, v);
         if(v.empty()) continue;
-        for(int i = 0; i < v.size(); i++) {
-            t_matrix[i][y] = v[i];
+        for(int i = 0; i < 10; i++) {
+            matrix[i][y] = i < v.size() ? v[i] : '.';
         }
         y++;
     }
-    memcpy(matrix, t_matrix, sizeof(matrix));
+    while(y < 15) {
+        for(int i = 0; i < 10; i++) {
+            matrix[i][y] = '.';
+        }
+        y++;
+    }
 }
 
 int main() {
@@ -66,25 +81,19 @@ int main() {
     scanf("%d", &t);
     int game_index = 1;
     while(t--) {
-        for(int i = 0; i < 10; i++) scanf("%s", matrix[i]);
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 15; j++) {
-                t_matrix[9 - i][j] = matrix[i][j];
-            }
-        }
-        memcpy(matrix, t_matrix, sizeof(matrix));
+        for(int i = 9; i >= 0; i--) scanf("%s", matrix[i]);
         int score = 0;
         int move_index = 1;
         int moved_balls = 0;
         printf("Game %d:\n\n", game_index++);
         while(true) {
             int x = -1, y = -1, max_chess_count = -1;
-            memcpy(t_matrix, matrix, sizeof(matrix));
+            memset(vis, 0, sizeof(vis));
             for(int i = 0; i < 10; i++) {
                 for(int j = 0; j < 15; j++) {
-                    if(t_matrix[i][j] != '.') {
+                    if(!vis[i][j] && matrix[i][j] != '.') {
                         int chess_count = 0, left_most = -1, bottom_most = -1;
-                        find_max_cluster(i, j, chess_count, left_most, bottom_most, t_matrix);
+                        find_max_cluster(i, j, chess_count, left_most, bottom_most);
                         if(chess_count > max_chess_count) {
                             x = bottom_most; y = left_most; max_chess_count = chess_count;
                         } else if(chess_count == max_chess_count) {
@@ -101,18 +110,8 @@ int main() {
             moved_balls += max_chess_count;
             printf("Move %d at (%d,%d): removed %d balls of color %c, got %d points.\n",
                    move_index++, x+1, y+1, max_chess_count, matrix[x][y], current_score);
-            max_chess_count = 0;
-            int left_most = -1, bottom_most = -1;
-            find_max_cluster(x, y, max_chess_count, left_most, bottom_most, matrix);
+            fill_dot(x, y, matrix[x][y]);
             regular_chessboard();
-            /*
-            for(int i = 0; i < 10; i++) {
-                for(int j = 0; j < 15; j++) {
-                    putchar(matrix[i][j]);
-                }
-                printf("\n");
-            }
-            */
         }
         if(moved_balls == 150) {
             score += 1000;
